2c6c14107473aa8db20107fc432f0c171334a05a
Harshini-ops <harshinin004@gmail.com>
2025-01-29 17:13:17 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,4 @@
+function showFunFact() {
+    const funFact = document.getElementById("fun-fact");
+    funFact.textContent = "Did you know? Inception's famous spinning top scene was shot without any CGI. It was a practical effect!";
+}
